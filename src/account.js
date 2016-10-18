43e36cd9cd6ca36c8d0e5545e2bb08f212be1b9b
mrmurtz <murtaza.s.abidi@gmail.com>
2016-10-18 14:29:03 +0100

    Transaction has if/else to store amount as either credit or debit, refactored statement print out

--- src/account.js
+++ src/account.js
@@ -9,25 +9,24 @@ Account.prototype = {
 
   deposit: function(amount){
     this._balance += amount;
-    this._transactionClass = new Transaction('deposit', amount, this._balance);
+    this._transactionClass = new Transaction("credit", amount, this._balance);
     this._history.push(this._transactionClass);
     // return this._transactionClass;
   },
 
   withdraw: function(amount){
     this._balance -= amount;
-    this._transactionClass = new Transaction('withdrawal', amount, this._balance);
+    this._transactionClass = new Transaction('debit', amount, this._balance);
     this._history.push(this._transactionClass);
     // return this._transactionClass;
   },
 
   printStatement: function(){
     console.log("History:");
+    console.log("Date       ||" + " Credit   || " + "  Debit   ||" + "  Balance   ||");
     var history = this._history;
     for (var i = 0; i < history.length; i++) {
-      console.log(history[i]._date + " || " + history[i]._type + " || " + history[i]._amount + " || " + history[i]._balance);
+      console.log(history[i]._date + " || " + history[i]._credit + " || " + history[i]._debit + " || " + history[i]._balance);
     }
-
   }
-
 };
--- src/transaction.js
+++ src/transaction.js
@@ -1,6 +1,14 @@
 function Transaction(type, amount, balance){
-  this._date = moment().format("DD/MM/YYYY");
-  this._type = type;
-  this._amount = amount;
-  this._balance = balance;
+  if (type == "credit") {
+    this._date = moment().format("DD/MM/YYYY");
+    this._credit = amount;
+    this._debit = 0;
+    this._balance = balance;
+  }
+  else {
+    this._date = moment().format("DD/MM/YYYY");
+    this._credit = 0;
+    this._debit = amount;
+    this._balance = balance;
+  }
 }
