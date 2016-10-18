0b0a9e172cafdf7a7a3186f9ed9bfd2acfcd398c
mrmurtz <murtaza.s.abidi@gmail.com>
2016-10-18 16:26:45 +0100

    Refactored to delete empty files and comments

--- src/account.js
+++ src/account.js
@@ -10,14 +10,12 @@ Account.prototype = {
     this._balance += amount;
     this._transactionClass = new Transaction("credit", amount, this._balance);
     this._history._history.push(this._transactionClass);
-    // return this._transactionClass;
   },
 
   withdraw: function(amount){
     this._balance -= amount;
     this._transactionClass = new Transaction('debit', amount, this._balance);
     this._history._history.push(this._transactionClass);
-    // return this._transactionClass;
   },
 
   printStatement: function(){
